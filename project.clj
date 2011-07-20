108de4f6e42bcfcc83523d399c2d65766f86bad3
smalls <small@cybertronic.com>
2011-07-20 14:28:52 -0700

    the front page is loading

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject start-clojure "1.0.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.2.1"]
-				[org.clojure/clojure-contrib "1.2.0"]
-  				 [compojure "0.6.4"]]
+	:description		"initial project"
+	:dependencies		[[org.clojure/clojure "1.2.1"]
+							[org.clojure/clojure-contrib "1.2.0"]
+  				 			[compojure "0.6.4"]]
 	:dev-dependencies	[[lein-ring "0.4.5"]]
-)
+	:ring	{:handler start-clojure.core/app})
--- src/start_clojure/core.clj
+++ src/start_clojure/core.clj
@@ -1,1 +1,12 @@
-(ns start-clojure.core)
+(ns start-clojure.core
+	(:use compojure.core)
+	(:require	[compojure.route :as route]
+				[compojure.handler :as handler]))
+
+(defroutes main-routes
+	(GET "/" [] "<h1>HI HI HI</h1>")
+	(route/resources "/")
+	(route/not-found "Page not found"))
+
+(def app
+	(handler/site main-routes))
