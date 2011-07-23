7bab2791841897ca2bf61f173db33e7c5d7dc422
smalls <small@cybertronic.com>
2011-07-22 18:06:31 -0700

    few more changes

--- src/start_clojure/core.clj
+++ src/start_clojure/core.clj
@@ -3,10 +3,23 @@
 	(:require	[compojure.route :as route]
 				[compojure.handler :as handler]))
 
+(defn index-page []
+	(str "hi hi hi"))
+
+
 (defroutes main-routes
-	(GET "/" [] "<h1>HI HI HI</h1>")
+	(GET "/" [] (index-page))
+
+	(POST "/post" []
+		(str "tbd"))
+	(GET "/post/:id" [id]
+		(str "<h1>post " id " </h1>"))
+	(PUT "/post/:id" [id]
+		(str "tbd id " id))
+
 	(route/resources "/")
 	(route/not-found "Page not found"))
 
 (def app
 	(handler/site main-routes))
+
