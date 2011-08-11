7334fe7ba9a27fac496a51f7a4b99b9d8d3e1a83
smalls <small@cybertronic.com>
2011-08-11 01:30:41 -0700

    real basic styling

--- src/start_clojure/templates.clj
+++ src/start_clojure/templates.clj
@@ -1,14 +1,17 @@
 (ns start-clojure.templates
-	(:require		[net.cgrand.enlive-html :as html]))
+	(:require		[net.cgrand.enlive-html :as html]
+					[clj-time.core :as clj-time]
+					[clj-time.format :as clj-time-format]
+					[clj-time.coerce :as clj-time-coerce]))
 
-(html/defsnippet post-snippet "start_clojure/templates/main.html" [:div.post]
-	[{:keys [title content]}]
-	[:.title] (html/content title)
-	[:.body] (html/content content))
+(def date-output-format (clj-time-format/formatter "dd MMM yyyy HH:mm"))
 
 (html/deftemplate main "start_clojure/templates/main.html"
 	[ctx]
 	[:p#blogname] (html/content (:blogname ctx))
 	[:head :title] (html/content (:blogname ctx))
-	[:div.posts] (html/clone-for [item (:posts ctx)]
-				   (html/content (post-snippet item))))
+	[:div.post] (html/clone-for [item (:posts ctx)]
+			[:.posttitle] (html/content (:title item))
+			[:.postdate] (html/content (clj-time-format/unparse date-output-format
+					(clj-time-coerce/from-date (:created_date item))))
+			[:.postbody] (html/content (:content item))))
