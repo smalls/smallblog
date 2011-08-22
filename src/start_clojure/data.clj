e014156709204b51315b869ed450a14c2edfc0f5
smalls <small@cybertronic.com>
2011-08-22 09:20:44 -0700

    add login info to the data api; core is still messed up

--- src/start_clojure/data.clj
+++ src/start_clojure/data.clj
@@ -6,8 +6,15 @@
 		bash$ createdb smallblog
 		bash$ psql smallblog -h localhost
 		psql$
+CREATE TABLE login (
+id SERIAL,
+email TEXT NOT NULL UNIQUE,
+password TEXT NOT NULL,
+PRIMARY KEY(id)
+);
 CREATE TABLE blog (
 id SERIAL,
+owner int NOT NULL REFERENCES login(id) ON DELETE CASCADE,
 title TEXT NOT NULL,
 created_date TIMESTAMP with time zone DEFAULT current_timestamp NOT NULL,
 PRIMARY KEY(id)
@@ -36,17 +43,34 @@ PRIMARY KEY(id)
 			}))
 
 
+; XXX only return if the password matches
+(defn get-login [email password]
+	(sql/with-connection *db*
+		(sql/with-query-results rs ["select * from login where email=?" email]
+			(first rs))))
+
+(defn make-login
+	"creates a new login and returns the id (instead of a populated object)"
+	[email password]
+	(sql/with-connection *db*
+		(sql/insert-record :login {:email email :password password})))
+
+(defn delete-login [id]
+	(sql/with-connection *db*
+		(sql/delete-rows :login ["id=?" id])))
+
 (defn get-blog [id]
 	(sql/with-connection *db*
 		(sql/with-query-results rs ["select * from blog where id=?" id]
 			(first rs))))
 
-(defn make-blog [title]
+(defn make-blog [login_id, title]
 	(sql/with-connection *db*
-		(let [id (sql/insert-record :blog {:title title})]
+		(let [id (sql/insert-record :blog {:title title :owner login_id})]
 			(get-blog id))))
 
-(defn delete-blog [id]
+(defn delete-blog [login_id, id]
+	; XXX check to make sure the user has permissions
 	(sql/with-connection *db*
 		(sql/delete-rows :blog ["id=?" id])))
 
--- test/start_clojure/test/data.clj
+++ test/start_clojure/test/data.clj
@@ -5,10 +5,25 @@
 	(:require	[start-clojure.data :as data]
 				[clj-sql.core :as sql]))
 
+(deftest login
+	"test basic creation and retrival of login rows"
+	[]
+	(let [login (str (now) "newlogin@foo.com")
+			password "somepassword"
+			loginid (data/make-login login password)]
+		(try
+			(let [loginobj (data/get-login login password)]
+				(is (= loginid (:id loginobj)))
+				(is (= login (:email loginobj)))
+				(is (= password (:password loginobj))))
+			(finally (data/delete-login loginid)))))
+
 (deftest post
 	"test basic creation of posts - including (make-post), (get-posts 1 0), and (get-post id)"
+	[]
 	(let [content (str "some content " (now))
-		blogid (:id (data/make-blog "blogname"))]
+			loginid (data/make-login (str (now) "@test.com") "password")
+			blogid (:id (data/make-blog loginid "blogname"))]
 	 	(try
 			(let [new-row (data/make-post blogid "text" content)]
 				(is (= content (:content new-row))))
@@ -18,19 +33,20 @@
 				(let [single-result (data/get-post blogid (:id (first result)))]
 					(is (= (get (first result) :content)
 							(get single-result :content)))))
-			(finally (data/delete-blog blogid)))))
+			(finally (data/delete-login loginid)))))
 
 (deftest post-cascade-delete
 	"test that the cascade delete removes nested posts when deleting a blog"
-	(let [blogid (:id (data/make-blog "blogname"))]
+	(let [loginid (data/make-login (str (now) "@test.com") "password")]
 		(try
-			(let [postid (:id (data/make-post blogid "title" "content"))]
-				(data/delete-blog blogid)
+			(let [blogid (:id (data/make-blog loginid "blogname"))
+					postid (:id (data/make-post blogid "title" "content"))]
+				(data/delete-blog loginid blogid)
 				(sql/with-connection data/*db*
 					(sql/with-query-results rs
 							["select * from post where id=?" postid]
 						(is (= 0 (count rs))))))
-			(finally (data/delete-blog blogid)))))
+			(finally (data/delete-login loginid)))))
 
 (deftest post-interval
 	"test getting posts over certain intervals"
@@ -38,7 +54,8 @@
 			content2 (str "second content " (now))
 			content3 (str "third content " (now))
 			content4 (str "fourth content " (now))
-			blogid (:id (data/make-blog "blogname"))]
+			loginid (data/make-login (str (now) "@test.com") "password")
+			blogid (:id (data/make-blog loginid "blogname"))]
 		(try
 			(data/make-post blogid "title" content1)
 			(data/make-post blogid "title" content2)
@@ -53,4 +70,4 @@
 				(is (= 2 (count result)))
 				(is (= content2 (get (first result) :content)))
 				(is (= content1 (get (second result) :content))))
-			(finally (data/delete-blog blogid)))))
+			(finally (data/delete-login loginid)))))
